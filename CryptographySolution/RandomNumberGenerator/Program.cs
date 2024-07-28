830b2995af827801874ce85f78a5a742324742e0
DESKTOP-V6IH109\dildar <dildarhussain997@gmail.com>
2024-07-28 12:24:27 +0400

    random number generator done

--- CryptographySolution/RandomNumberGenerator/Program.cs
+++ CryptographySolution/RandomNumberGenerator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RandomNumberGenerator ;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Random Number Generator in .Net");
+		Console.WriteLine("-------------------------------");
+		Console.WriteLine();
+
+		// this will generate 10 random number 
+		for (int i = 0; i < 10; i++)
+		{
+			// this will generate a 32 bit random number and print it to screen
+			Console.WriteLine("Random Number " + i + " : " + Convert.ToBase64String(RandomNumGen.GenerateRandomNumber(32)));
+		}
+		Console.ReadLine();
+	}
+}
---
+++ CryptographySolution/RandomNumberGenerator/RandomNumGen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
+
+namespace RandomNumberGenerator
+{
+	public static class RandomNumGen
+	{
+		// this funciton generates true random number using cryptography based on length provided
+		public static byte[] GenerateRandomNumber(int lenth)
+		{
+			using (var randomNumbeGenerator =  new RNGCryptoServiceProvider())
+			{
+				var randomNumber = new byte[lenth];
+				randomNumbeGenerator.GetBytes(randomNumber);
+				return randomNumber;
+			}
+		}
+
+	}
+}
